df179d9f2055e5079a6b8e6cc007ed733aabf180
unknown <rodrigo.m.flores.jr@gmail.com>
2012-09-27 14:43:58 +0800

    new update with table dump

---
+++ dump file/provider.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS `ricemill`.`provider`;
+CREATE TABLE  `ricemill`.`provider` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
---
+++ dump file/stockin_provider.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS `ricemill`.`stockin_provider`;
+CREATE TABLE  `ricemill`.`stockin_provider` (
+  `stockin_id` int(10) unsigned NOT NULL,
+  `provider_id` int(10) unsigned NOT NULL,
+  KEY `FK_stockin_provider_1` (`stockin_id`),
+  KEY `FK_stockin_provider_2` (`provider_id`),
+  CONSTRAINT `FK_stockin_provider_1` FOREIGN KEY (`stockin_id`) REFERENCES `stock_in` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_stockin_provider_2` FOREIGN KEY (`provider_id`) REFERENCES `provider` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
